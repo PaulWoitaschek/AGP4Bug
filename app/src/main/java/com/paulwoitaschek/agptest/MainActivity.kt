4d4a5d1e6ee934330ba39e39a470184a82ee6b06
Paul Woitaschek <woitaschek@posteo.de>
2020-06-03 06:58:50 +0200

    Something

--- app/src/main/java/com/paulwoitaschek/agptest/MainActivity.kt
+++ app/src/main/java/com/paulwoitaschek/agptest/MainActivity.kt
@@ -1,43 +1,26 @@
 package com.paulwoitaschek.agptest
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import java.io.IOException
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    val retrofit = Retrofit.Builder()
+      .baseUrl("https://www.google.de/")
+      .validateEagerly(true)
+      .build()
+    retrofit.create(MyInterface::class.java)
+  }
 
-    val handler = Handler()
+  interface MyInterface {
 
-    OkHttpClient()
-      .newCall(
-        Request.Builder()
-          .url("https://www.google.com/")
-          .get()
-          .build()
-      )
-      .enqueue(object : Callback {
-        override fun onFailure(call: Call, e: IOException) {
-          handler.post {
-            textView.text = e.toString()
-          }
-        }
-
-        override fun onResponse(call: Call, response: Response) {
-          handler.post {
-            textView.text = response.body!!.string()
-          }
-        }
-      })
+    @GET("meop")
+    fun test(date: LocalDate): Call<Void?>
   }
 }
